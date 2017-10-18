92ca72d37bf74f610e9e4e9680efac45e0234bc8
Manoj Chaurasiya <manoj.chaurasiya@gmail.com>
2017-10-19 00:24:27 +0530

    new Changes

--- TypeScript/first.js
+++ TypeScript/first.js
@@ -1,6 +1,7 @@
-"use strict";
 function user(name) {
     return " Hello" + name + " !!!";
 }
 var usr = "Manoj Chaurasiya ";
+var i = "not a number";
+console.log(i);
 document.body.innerHTML = user(usr);
--- TypeScript/first.ts
+++ TypeScript/first.ts
@@ -3,5 +3,6 @@ function user(name:string){
 }
 
 var usr = "Manoj Chaurasiya ";
-
+var i : number = "not a number";
+console.log(i);
 document.body.innerHTML = user(usr);
