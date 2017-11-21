5bc30a4991ad912dede98bcc82adc259af82243c
Manoj Chaurasiya <manoj.chaurasiya@gmail.com>
2017-11-21 22:44:28 +0530

    Array Function added

---
+++ ES6/Array_function.js
@@ -0,0 +1,9 @@
+/**
+ * ES6 : Array Function
+ */
+var arr = [1,2,3,4,5,6,7,8];
+// Searching function
+var f = arr.find(x => x > 3) // 4
+var ind = arr.findIndex(x => x > 3) // 2
+console.log(f);
+console.log(ind);
