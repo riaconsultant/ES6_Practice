3fcc1730f77b7f6eb0e3923ce11b36795579fec8
Manoj Chaurasiya <manoj.chaurasiya@gmail.com>
2017-11-21 22:50:54 +0530

    Added arrow functions

--- ES6/Array_function.js
+++ ES6/Array_function.js
@@ -7,3 +7,7 @@ var f = arr.find(x => x > 3) // 4
 var ind = arr.findIndex(x => x > 3) // 2
 console.log(f);
 console.log(ind);
+
+// Arrow Functions
+var plus = arr.map(v => v+1);
+console.log(plus);
